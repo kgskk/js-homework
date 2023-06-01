b96dcf8b2e6407da92d2d4ff976bf1bdc24318d1
kgskk <131375173+kgskk@users.noreply.github.com>
2023-06-01 21:39:53 +0300

    Add files via upload

---
+++ site/code.js
@@ -0,0 +1,4 @@
+document.write("<div style='height: 200px; width: 800px; display: block' class='header'> </div>") 
+document.write("<div style='height: 400px; width: 100px; display: inline-block' class='nav'> </div>") 
+document.write("<div style='height: 400px; width: 700px; display: inline-block' class='section'> </div>") 
+document.write("<div style='height: 200px; width: 800px; display: inline-block' class='footer'> </div>") 
